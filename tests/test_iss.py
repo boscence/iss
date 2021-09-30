d8cb8e194a42bb52b49887db0a9f875471b98039
Andrew Boscence <andrew@yhmarkets.com>
2021-09-30 18:36:16 +0800

    Added tests

---
+++ tests/test_iss.py
@@ -0,0 +1,10 @@
+import pytest
+from devup.src.get_iss_location import get_iss_location 
+
+class TestOne:
+
+    def test_iss(self):
+        is_dict = get_iss_location()
+        isinstance(is_dict,dict) 
+
+    
